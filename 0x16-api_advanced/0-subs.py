2e0946dbf3e73261f5d8e8bdeb74021ce4504191
Musawenkosistar <musawenkosi2504@gmail.com>
2024-02-15 21:54:02 +0000

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,26 +4,23 @@ Write a Python script that queries the Reddit API and returns the number of
 subscribers (not active users, total subscribers) for a given subreddit.
 If an invalid subreddit is given, the function should return 0
 """
-
+import json
+# import pprint
 import requests
+import sys
+
+headers = {
+    'User-Agent': 'My User Agent 1.0'
+}
+
 
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    user_agent = "ALX Project/1.0 by /u/Musawenkosistar"  
-    headers = {'User-Agent': user_agent}
-    
+    """function that returns the number of subscribers"""
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)  
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        elif response.status_code == 404:
-            print(f"Subreddit '{subreddit}' not found.")
-            return 0
-        else:
-            print("Error occurred while fetching data from Reddit API.")
-            return 0
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
+        url = 'https://www.reddit.com/r/'
+        response = requests.get(url + subreddit + "/about.json",
+                                headers=headers, allow_redirects=False)
+        return response.json()['data']['subscribers']
+        # pprint.pprint(response.json()['data']['subscribers'])
+    except:
         return 0

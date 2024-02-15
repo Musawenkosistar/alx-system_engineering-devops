d3f9697073533a454f66a1e368b441ef724c7122
Musawenkosistar <musawenkosi2504@gmail.com>
2024-02-15 21:36:08 +0000

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,26 @@ Contains the number_of_subscribers function
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    user_agent = "ALX Project/1.0 by /u/Musawenkosistar"  
+    headers = {'User-Agent': user_agent}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)  
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        elif response.status_code == 404:
+            print(f"Subreddit '{subreddit}' not found.")
+            return 0
+        else:
+            print("Error occurred while fetching data from Reddit API.")
+            return 0
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': 'ALX Project:v1.0.0 by /u/Musawenkosistar'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+
+subreddit = "python"
+print(f"Number of subscribers in r/{subreddit}: {number_of_subscribers(subreddit)}")

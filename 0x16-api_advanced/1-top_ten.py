2e70f79b46bc7177202ef000d22ba3b283e44711
Musawenkosistar <musawenkosi2504@gmail.com>
2024-02-15 22:07:01 +0000

    0x16-api_advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
-"""Contains top_ten function"""
+"""
+Write a Python script that queries the Reddit API and returns the number of
+subscribers (not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0
+"""
+import json
+# import pprint
 import requests
+import sys
+
+headers = {
+    'User-Agent': 'ALX Project/1.0 by /u/Musawenkosistar'
+}
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    """function that returns the titles of the first 10 hot posts"""
+    try:
+        url = 'https://www.reddit.com/r/'
+        response = requests.get(url + subreddit + "/hot.json?limit=10",
+                                headers=headers, allow_redirects=False)
+        myArray = [element['data']['title'] for element in response.
+                   json()['data']['children']]
+        print(*myArray, sep='\n')
+        # pprint.pprint(response.json())
+    except:
+        print(None)

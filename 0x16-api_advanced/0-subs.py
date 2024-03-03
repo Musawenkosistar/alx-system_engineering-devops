31b104fce8f08bde4cc9c5f32a107c43ca2cd2e3
Musawenkosistar <musawenkosi2504@gmail.com>
2024-03-02 23:44:45 -0800

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-"""
-    this module contains the function number_of_subscribers
-"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
-from sys import argv
 
 
-def number_of_subscribers(subreddit):
-    """
-        returns the number of subscribers for a given subreddit
-    """
-    user = {'User-Agent': 'ALX Project'}
-    url = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=user).json()
-    try:
-        return url.get('data').get('subscribers')
-    except Exception:
+def number_of_subscribera(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = { 'User-Agent': 'ALX Project/1.0' }
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get('data')
+    return results.get('subscribers')
+    

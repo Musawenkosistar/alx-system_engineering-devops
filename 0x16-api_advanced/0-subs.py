bab1035615c5a61576272d7deb7430b0c64a9c4f
Musawenkosistar <musawenkosi2504@gmail.com>
2024-06-21 07:54:52 +0000

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
 """
-   use reddit api to print the number of subscribers of a subreddit
+Script that queries subscribers on a given Reddit subreddit.
 """
+
 import requests
 
+
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        "User-Agent": "ALX Project/1.0"  # Set your own user-agent string here
-    }
-    params = {
-        'raw_json': 1
-    }
-    
-    try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-        if response.status_code == 404:
-            print(f"Subreddit '{subreddit}' not found.")
-            return 0
-        elif response.status_code != 200:
-            print(f"Error fetching data: Status code {response.status_code}")
-            return 0
-        
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "ALX Project/1.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
-    
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching data: {e}")
+    else:
         return 0

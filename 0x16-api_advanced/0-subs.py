a2a55560d21c8706495638f4c9553075b5d7d801
Musawenkosistar <musawenkosi2504@gmail.com>
2024-08-13 10:41:18 -0700

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/subreddit/about.json".format(subreddit)
-    headers = {"User-Agent": "Musawenkosistar by u/Musawenkosistar"}
+    headers = {"User-Agent": "Musawenkosistar v1.0 by /u/Musawenkosistar"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
